36817679ca7b5e91f4febfb4ce2b77386cb4519d
PGallagher93 <Pgallagher93@hotmail.com>
2024-10-24 18:46:53 +0100

    adds basic mui header

--- workout-app-fe/src/App.jsx
+++ workout-app-fe/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import Header from "./Components/Header/Header.jsx"
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
---
+++ workout-app-fe/src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import NavBar from "./NavBar.jsx"
+
+const Header = () =>{
+    return (
+        <header>
+            <NavBar/>
+        </header>
+    )
+}
+export default Header
