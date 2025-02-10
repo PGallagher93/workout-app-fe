0ceb3072474de4885f4bff0aa7835c3fa3b9adda
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-10 16:18:01 +0000

    changes nav from grid to flex

--- workout-app-fe/src/Components/Header/NavBar.jsx
+++ workout-app-fe/src/Components/Header/NavBar.jsx
@@ -10,10 +10,14 @@ const NavBar = ({user})=> {
  
 
   return (
-   <nav className="grid grid-cols-4 w-full ">
-    <Bars3Icon className="text-orange-400"></Bars3Icon>
-    <h1 className="text-orange-400 col-span-2">{user.username ? (user.username): "Welcome!"}</h1>
+   <nav className="flex justify-around w-full p-2">
+    <button>
+    <Bars3Icon className="text-orange-400 h-10"></Bars3Icon>
+    </button>
+    
+    <h1 className="text-orange-400">{user.username ? (user.username): "Welcome!"}</h1>
     {!user.username ? (
+
       <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
       <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
   </div>
