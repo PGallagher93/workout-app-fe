c62f446e767fef86ebe9ec0c04ca296df66346bb
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-10 17:14:26 +0000

    adds flex around to nav bar

--- workout-app-fe/src/Components/Header/NavBar.jsx
+++ workout-app-fe/src/Components/Header/NavBar.jsx
@@ -1,31 +1,21 @@
-import {useState, useEffect} from "react"
-import {Bars3Icon} from '@heroicons/react/24/outline'
+import { useState, useEffect } from "react";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
-const NavBar = ({user})=> {
-  console.log(user)
-  
-  
-
-
- 
+const NavBar = ({ user }) => {
+  console.log(user);
 
   return (
-   <nav className="flex justify-around w-full p-2">
-    <button>
-    <Bars3Icon className="text-orange-400 h-10"></Bars3Icon>
-    </button>
-    
-    <h1 className="text-orange-400">{user.username ? (user.username): "Welcome!"}</h1>
-    {!user.username ? (
+    <nav className="flex justify-around w-full p-2">
+      <button></button>
 
-      <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-      <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-  </div>
-    ) : (<img class="w-10 h-10 rounded-full" src={user.avatar} alt="Rounded avatar"></img>
-    )}
-    
+      <img
+        class="w-10 h-10 rounded-full"
+        src={user.avatar}
+        alt="Rounded avatar"
+      ></img>
 
-   </nav>
+      <h1 className="text-orange-400"> {user.username}</h1>
+    </nav>
   );
-}
-export default NavBar
+};
+export default NavBar;
