53d898c288e0ef0a8393582ee169c61d944aef34
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-10 17:51:08 +0000

    adds imports for user context to app.jsx

--- workout-app-fe/src/App.jsx
+++ workout-app-fe/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import Header from "./Components/Header/Header.jsx"
 import './App.css'
 import SpaceWolvesAvatar from "./Avatars/SpaceWolves.jpeg"
-
+import {useContext} from 'react'
+import { UserContext } from './contexts/userContext.js'
 function App() {
  
   const [user, setUser] = useState({username:"Sigismund", userId: 1, avatar: SpaceWolvesAvatar })
