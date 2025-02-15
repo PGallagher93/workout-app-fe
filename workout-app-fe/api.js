d3157fece7c2271431fc2a22e98e744e14808f28
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-15 16:23:18 +0000

    adds fetch user workouts to api.js

--- workout-app-fe/api.js
+++ workout-app-fe/api.js
@@ -4,4 +4,8 @@ const fetchPremadeWorkouts = () => {
     return axios.get(`https://pg-workout-app.onrender.com/api/premades`)
 }
 
+const fetchUserWorkouts = (id) =>{
+    return axios.get(`https://pg-workout-app.onrender.com/api/workouts/${id}`)
+}
+
 export {fetchPremadeWorkouts}
