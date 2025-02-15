8db4e715b4956a93beab0fa886fed73e2c8b6d64
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-15 22:53:10 +0000

    adds rounded corners to containertile

--- workout-app-fe/src/Components/ContainerTile.jsx
+++ workout-app-fe/src/Components/ContainerTile.jsx
@@ -1,7 +1,7 @@
 const ContainerTile = (title) => {
     
     return (
-        <div className="bg-orange-400 h-100vh w-25vw">
+        <div className="bg-orange-400 min-h-20 w-25vw p-1 rounded">
             <p>{title.title}</p>
         </div>
     )
