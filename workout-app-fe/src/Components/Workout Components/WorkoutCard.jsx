9ba64999e8566880e2fa1ee9b483827bd4940f38
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-16 11:17:48 +0000

    adds button to workoutCard

--- workout-app-fe/src/Components/Workout Components/WorkoutCard.jsx
+++ workout-app-fe/src/Components/Workout Components/WorkoutCard.jsx
@@ -1,7 +1,8 @@
 const WorkoutCard = (title)=>{
 return (
-    <div>
-    <p> hehe</p>
+    <div className="bg-orange-400 min-h-20 w-25vw p-1 rounded m-12">
+    <span>{title.title}</span>
+    <button >add</button>
     </div>
 )
 }
