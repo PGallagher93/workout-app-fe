c7e7f86242da7f27c500da401043fe087bb5c21f
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-18 15:24:15 +0000

    adds basic div for workoutstatscontainer

--- workout-app-fe/src/Components/Workout Components/WorkoutStatsContainer.jsx
+++ workout-app-fe/src/Components/Workout Components/WorkoutStatsContainer.jsx
@@ -1,6 +1,9 @@
 
 const WorkoutStatsContainer = ({}) => {
-
+return (
+<div className="bg-orange-400 min-h-20 w-25vw p-1 rounded m-12">
+<p>hey</p>
+</div>)
 }
 
 export default WorkoutStatsContainer
