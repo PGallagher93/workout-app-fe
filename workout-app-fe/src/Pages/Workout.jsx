fb56aaa6092522701969d591cf49f2b63bcc7d8f
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-18 13:26:19 +0000

    adds useparams to single workout component

--- workout-app-fe/src/Pages/Workout.jsx
+++ workout-app-fe/src/Pages/Workout.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom"
 const Workout = () =>{
+
+    const {id} = useParams()
+    console.log(id)
     return (
         <div className="pt-14 bg-zinc-800 h-screen">
 
