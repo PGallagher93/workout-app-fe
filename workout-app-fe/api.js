c6a936e5ac203194f1f4c0a0503d809a8dc48d4e
PGallagher93 <Pgallagher93@hotmail.com>
2025-02-26 13:52:07 +0000

    finishes postworkoutweightstat function

--- workout-app-fe/api.js
+++ workout-app-fe/api.js
@@ -12,8 +12,9 @@ const fetchSingleWorkout = (id) => {
     return axios.get(`https://pg-workout-app.onrender.com/api/user/workouts/${id}`)
 }
 
-const PatchWorkoutWeightStat = (stat_id, weight) =>{
-    
+const patchWorkoutWeightStat = (stat_id, weight) =>{
+    return axios.patch(`https://pg-workout-app.onrender.com/api/user/workouts/workout_stats`,
+    {stat_id: stat_id, weight: weight})
 }
 
-export {fetchPremadeWorkouts, fetchUserWorkouts, fetchSingleWorkout}
+export {fetchPremadeWorkouts, fetchUserWorkouts, fetchSingleWorkout, patchWorkoutWeightStat}
