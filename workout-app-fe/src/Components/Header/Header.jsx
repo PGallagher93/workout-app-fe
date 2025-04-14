73039880329961f3ecb37938cbda58cca63cfb58
PGallagher93 <Pgallagher93@hotmail.com>
2025-04-14 14:08:37 +0100

    adds color gradient to header bg

--- workout-app-fe/src/Components/Header/Header.jsx
+++ workout-app-fe/src/Components/Header/Header.jsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar.jsx"
 
 const Header = () =>{
     return (
-        <header className="h-14 bg-black flex" >
+        <header className="h-14 bg-gradient-to-r from-black to-gray-600 flex" >
             <NavBar />
         </header>
     )
