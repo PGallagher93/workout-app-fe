fb59a7bace36cee5f05e872c20a2a1c57dfb1f26
PGallagher93 <Pgallagher93@hotmail.com>
2025-04-28 13:18:59 +0100

    adds pagging on x axis for page container

--- workout-app-fe/src/Components/Basic Components/PageContainer.jsx
+++ workout-app-fe/src/Components/Basic Components/PageContainer.jsx
@@ -2,7 +2,7 @@
 
 const PageContainer = ({children}) => {
     return (
-        <div className="pt-14  bg-black text-white h-full min-h-screen">
+        <div className="pt-14 px-5 bg-black text-white h-full min-h-screen">
           {children}
         </div>
     )
