7e4ced4af844fea5fc2513427338823fd69c3598
PGallagher93 <Pgallagher93@hotmail.com>
2025-04-30 16:15:56 +0100

    adds padding top to coursel

--- workout-app-fe/src/Components/Homepage Components/Carousel.jsx
+++ workout-app-fe/src/Components/Homepage Components/Carousel.jsx
@@ -1,6 +1,6 @@
 const Carousel = ({children}) =>{
     return(
-        <section className=" bg-black mt-4 pl-[20%] flex w-full gap-4 pb-5 snap-x overflow-x-auto">
+        <section className=" bg-black mt-4 pl-[20%] flex w-full gap-4 pb-5 pt-[50%] md:pt-[25%] snap-x overflow-x-auto">
                {children}
         </section>
     )
